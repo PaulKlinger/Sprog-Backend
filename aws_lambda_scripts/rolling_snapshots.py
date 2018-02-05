bb612881afd9a6f15394d7cc76214aa0c7391298
Paul Klinger <klinger.paul@gmail.com>
2018-02-05 21:59:41 +0100

    updated snapshot AWS Lambda script
    use python 3, retry deletion more often

--- aws_lambda_scripts/rolling_snapshots.py
+++ aws_lambda_scripts/rolling_snapshots.py
@@ -4,16 +4,19 @@ from datetime import timedelta, datetime
 
 print('Loading function')
 
+
 def lambda_handler(event, context):
-
     retention_days = 2
+    deletion_days = 14
 
-    print("AWS snapshot backups stated at %s...\n" % datetime.now())
+    print("AWS snapshot backups started at %s...\n" % datetime.now())
     ec = boto3.client('ec2')
     snapshot = ec.create_snapshot(
             VolumeId="vol-0e3fe6bd947540db4",
         )
-    if snapshot:
+    print(snapshot)
+    if snapshot['State'] != "error":
+        print("Snapshot creation in progress, deleting old snapshots")
         delete_fmt = (datetime.today() + timedelta(days=retention_days)).strftime("%Y-%m-%d")
         ec.create_tags(
             Resources=[snapshot["SnapshotId"]],
@@ -21,14 +24,13 @@ def lambda_handler(event, context):
                 {'Key': 'DeleteOn', 'Value': delete_fmt},
             ]
         )
-        today = datetime.today().strftime('%Y-%m-%d')
-        yest = (datetime.today()-timedelta(days=1)).strftime('%Y-%m-%d')
-        yyest = (datetime.today()-timedelta(days=2)).strftime('%Y-%m-%d')
+        deletion_days = [(datetime.today()-timedelta(days=i)).strftime('%Y-%m-%d') for i in range(deletion_days)]
         filters = [
             {'Name': 'tag-key', 'Values': ['DeleteOn']},
-            {'Name': 'tag-value', 'Values': [today, yest, yyest]},
+            {'Name': 'tag-value', 'Values': deletion_days},
         ]
         snapshot_response = ec.describe_snapshots(OwnerIds=["681414469766"], Filters=filters)
+        print("Found {} old snapshots to delete.".format(len(snapshot_response['Snapshots'])))
         for snap in snapshot_response['Snapshots']:
             print("Deleting snapshot %s" % snap['SnapshotId'])
             ec.delete_snapshot(SnapshotId=snap['SnapshotId'])
