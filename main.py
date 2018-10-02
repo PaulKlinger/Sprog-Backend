fb2d38f36f111b76b601179910392dfd31388844
Paul Klinger <klinger.paul@gmail.com>
2018-10-02 20:43:32 +0200

    added rss feed generation

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from sprog_auth import passwords
 user_name = "Poem_for_your_sprog"
 latex_template_filename = "templates/sprog.tex.mako"
 html_template_filename = "templates/sprog.html.mako"
+rss_template_filename = "templates/sprog.rss.mako"
 latexfile = "sprog.tex"
 tmpdir = "tmp"
 
 if __name__ == "__main__":
     sprog = Sprog(user_name=user_name,
                   latex_template_filename=latex_template_filename, html_template_filename=html_template_filename,
+                  rss_template_filename=rss_template_filename,
                   tmpdir=tmpdir, latexfile=latexfile, passwords=passwords)
     sprog.run()
